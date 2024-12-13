ca514115acf3ee10bf124d47002d4a882fe30dba
Malin Hanak <malin.hanak@smartrefill.se>
2024-12-13 19:47:11 +0100

    Setting up a ClientProvider wrapper

--- src/app/challenge-1/page.tsx
+++ src/app/challenge-1/page.tsx
@@ -1,5 +1,15 @@
+'use client'
+
 import { ProductGrid } from '@/components/ProductGrid/ProductGrid'
+import { ProductGridWithInifinitScroll } from '@/components/ProductGrid/ProductGridWithInifinitScroll'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function Challenge1() {
-  return <ProductGrid />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ProductGridWithInifinitScroll />
+    </QueryClientProvider>
+  )
 }
